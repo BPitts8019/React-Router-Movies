0d085e2fa3361c0b68129d49bbd042fcac48052e
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-06 16:26:03 -0700

    Updated to use MovieCard.js

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from "react-router-dom";
 import axios from 'axios';
+import MovieCard from './MovieCard.js';
 
 const MovieList = props => {
    const [movies, setMovies] = useState([])
@@ -21,35 +22,36 @@ const MovieList = props => {
 
    return (
       <div className="movie-list">
-         {movies.map(movie => (
-            <MovieDetails  key={movie.id} movie={movie} />
+         {movies.map((movie, index) => (
+            // <MovieDetails  key={movie.id} movie={movie} />
+            <Link to={`/movies/${movie.id}`} key={index}><MovieCard {...movie}/></Link>
          ))}
       </div>
    );
 }
 
-function MovieDetails({ movie }) {
-   const { id, title, director, metascore, stars } = movie;
-   return (
-      <Link to={`/movies/${id}`}>
-         <div className="movie-card">
-            <h2>{title}</h2>
-            <div className="movie-director">
-               Director: <em>{director}</em>
-            </div>
-            <div className="movie-metascore">
-               Metascore: <strong>{metascore}</strong>
-            </div>
-            <h3>Actors</h3>
+// function MovieDetails({ movie }) {
+//    const { id, title, director, metascore, stars } = movie;
+//    return (
+//       <Link to={`/movies/${id}`}>
+//          <div className="movie-card">
+//             <h2>{title}</h2>
+//             <div className="movie-director">
+//                Director: <em>{director}</em>
+//             </div>
+//             <div className="movie-metascore">
+//                Metascore: <strong>{metascore}</strong>
+//             </div>
+//             <h3>Actors</h3>
 
-            {stars.map(star => (
-               <div key={star} className="movie-star">
-                  {star}
-               </div>
-            ))}
-         </div>
-      </Link>
-   );
-}
+//             {stars.map(star => (
+//                <div key={star} className="movie-star">
+//                   {star}
+//                </div>
+//             ))}
+//          </div>
+//       </Link>
+//    );
+// }
 
 export default MovieList;
